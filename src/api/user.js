e443420c3285626c442cb635816601b830116647
chim Yakov Barzesky <chaimyakovbar@gmail.com>
2024-09-22 14:57:20 +0300

    2.7

--- src/api/user.js
+++ src/api/user.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
-// const baseurl = 'https://racheldatabase.onrender.com/user';
-const baseurl = 'https://racheldatabase2.onrender.com/user';
+const baseurl = 'https://racheldatabase.onrender.com/users';
+// const baseurl = 'https://racheldatabase2.onrender.com/user';
+// const baseurl = 'https://racheldatabase2.onrender.com/';
+// const baseurl = 'http://localhost:3010'
+
 
 export const getUsers = async () => await axios.get(baseurl);
 
 export const postUsers = async (data) => {
-    return await axios.post(baseurl , data)
+    return await axios.post(baseurl, data)
 };
 
 export const deleteUsers = async (id) => {
     return await axios.delete(`${baseurl}/${id}`);
 };
 
- 
- 
+
